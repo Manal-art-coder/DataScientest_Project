a476e93c7b614ab709ea0853fd123a43d37b87da
Manal-art-coder <manal.jewa@gmail.com>
2025-02-17 13:27:32 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,89 +3,25 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-df=pd.read_csv(r"C:\Users\manal\Desktop\Projets\Streamlit\train.csv")
+df=pd.read_csv(r"C:\Users\manal\Downloads\bank.csv")
 df.head()
-st.title("Projet de classification binaire Titanic")
+st.title("Prédiction du succès d'une campagne Marketing")
 st.sidebar.title("Sommaire")
-pages=["Exploration", "DataVizualization", "Modélisation"]
+pages=["Contexte et objectif", "Exploration du jeu de données", "Datavisulaization", "Préparation des données", "Modélisation", "Résultat et analyse"]
 page=st.sidebar.radio("Aller vers", pages)
 if page == pages[0] : 
-  st.write("### Introduction")
+  st.write("### Contexte et objectif")
+  st.write("Ce projet a été réalisé dans le cadre de notre formation en Data Analyst avec l’organisme Datascientest. L’objectif est de prédire le succès d’une campagne marketing en analysant les facteurs influençant la souscription des clients à une offre spécifique. La variable cible étant la souscription ou non à l’offre, nous utilisons l’apprentissage supervisé pour résoudre ce problème de classification binaire. À partir de données labellisées, nous cherchons à identifier les profils les plus susceptibles de souscrire, afin d’optimiser les actions marketing et réduire les coûts de la campagne. Le jeu de données utilisé provient de la plateforme Kaggle et est accessible à l’adresse suivante : Lien vers le dataset. Ce Streamlit retrace notre démarche, depuis l’exploration et le prétraitement des données jusqu’à la modélisation finale. Il permet de visualiser les différentes étapes du projet, d’analyser les variables explicatives sélectionnées et de tester plusieurs algorithmes de Machine Learning afin d’identifier le modèle le plus performant.")
+if page == pages[1] : 
+  st.write("### Exploration du jeu de données")
   st.dataframe(df.head(10))
   st.write(df.shape)
   st.dataframe(df.describe())
 if st.checkbox("Afficher les NA") :
   st.dataframe(df.isna().sum())
-if page == pages[1] : 
-  st.write("### DataVizualization")
-  fig = plt.figure()
-  sns.countplot(x = 'Survived', data = df)
-  st.pyplot(fig)
-  fig = plt.figure()
-  sns.countplot(x = 'Sex', data = df)
-  plt.title("Répartition du genre des passagers")
-  st.pyplot(fig)
-  fig = plt.figure()
-  sns.countplot(x = 'Pclass', data = df)
-  plt.title("Répartition des classes des passagers")
-  st.pyplot(fig)
-  fig = sns.displot(x = 'Age', data = df)
-  plt.title("Distribution de l'âge des passagers")
-  st.pyplot(fig)
-  fig = plt.figure()
-  sns.countplot(x = 'Survived', hue='Sex', data = df)
-  st.pyplot(fig)
-  fig = sns.catplot(x='Pclass', y='Survived', data=df, kind='point')
-  st.pyplot(fig)
-  fig = sns.lmplot(x='Age', y='Survived', hue="Pclass", data=df)
-  st.pyplot(fig)
-  fig, ax = plt.subplots()
-  df_num=df.select_dtypes(include=['int64', 'float64'])
-  sns.heatmap(df_num.corr(), ax=ax)
-  st.write(fig)
 if page == pages[2] : 
-  st.write("### Modélisation")
-  df = df.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1)
-  y = df['Survived']
-  X_cat = df[['Pclass', 'Sex',  'Embarked']]
-  X_num = df[['Age', 'Fare', 'SibSp', 'Parch']]
-  for col in X_cat.columns:
-     X_cat[col] = X_cat[col].fillna(X_cat[col].mode()[0])
-  for col in X_num.columns:
-     X_num[col] = X_num[col].fillna(X_num[col].median())
-  X_cat_scaled = pd.get_dummies(X_cat, columns=X_cat.columns)
-  X = pd.concat([X_cat_scaled, X_num], axis = 1)
+  st.write("### DataVisualization")
+  fig = plt.figure()
+  sns.countplot(x = 'deposit', data = df)
+  st.pyplot(fig)
   
-  from sklearn.model_selection import train_test_split
-  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
-  from sklearn.preprocessing import StandardScaler
-  scaler = StandardScaler()
-  X_train[X_num.columns] = scaler.fit_transform(X_train[X_num.columns])
-  X_test[X_num.columns] = scaler.transform(X_test[X_num.columns])
-  from sklearn.ensemble import RandomForestClassifier
-  from sklearn.svm import SVC
-  from sklearn.linear_model import LogisticRegression
-  from sklearn.metrics import confusion_matrix
-  def prediction(classifier):
-    if classifier == 'Random Forest':
-        clf = RandomForestClassifier()
-    elif classifier == 'SVC':
-        clf = SVC()
-    elif classifier == 'Logistic Regression':
-        clf = LogisticRegression()
-    clf.fit(X_train, y_train)
-    return clf
-  def scores(clf, choice):
-    if choice == 'Accuracy':
-        return clf.score(X_test, y_test)
-    elif choice == 'Confusion matrix':
-        return confusion_matrix(y_test, clf.predict(X_test))
-  choix = ['Random Forest', 'SVC', 'Logistic Regression']
-  option = st.selectbox('Choix du modèle', choix)
-  st.write('Le modèle choisi est :', option)
-  clf = prediction(option)
-  display = st.radio('Que souhaitez-vous montrer ?', ('Accuracy', 'Confusion matrix'))
-  if display == 'Accuracy':
-    st.write(scores(clf, display))
-  elif display == 'Confusion matrix':
-    st.dataframe(scores(clf, display))
